419ca129a627db5d1c49ec7771b9d41418b5a03b
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-05-12 14:21:33 +0300

    task solving

--- script.js
+++ script.js
@@ -20,7 +20,6 @@
 //document.write(answers);
 
 
-
 /* Задание на урок:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на
@@ -140,9 +139,6 @@
 4) Потренироваться и переписать Цикл еще двумя способами*/
 
 
-
-
-
 // Код возьмите из предыдущего домашнего задания
 
 // const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -189,7 +185,6 @@
 // } while (i < 2);
 
 
-
 // if (personalMovieDB.count < 10) {
 //     alert("Просмотрено довольно мало фильмов");
 // } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
@@ -201,8 +196,6 @@
 // }
 
 // console.log(personalMovieDB);
-
-
 
 
 /* Задание на урок:
@@ -363,17 +356,17 @@ P.S. Функции вызывать не обязательно*/
 // console.log(personalMovieDB);
 
 
-let object = {
-    name: 'Andrey',
-    family: 'Nikitin',
-    age: 50,
-    children: {
-        daughter1: 'Darya',
-        daughter2: 'Anisiya',
-        son1: 'Ivan',
-        son2: 'Timofey'
-    }
-};
+// let object = {
+//     name: 'Andrey',
+//     family: 'Nikitin',
+//     age: 50,
+//     children: {
+//         daughter1: 'Darya',
+//         daughter2: 'Anisiya',
+//         son1: 'Ivan',
+//         son2: 'Timofey'
+//     }
+// };
 
 // console.log(Object.keys(object));
 // console.log(Object.keys(object).length);
@@ -382,10 +375,10 @@ let object = {
 
 // Деструктуризация объекта
 
-const {daughter2, son2} = object.children;
-console.log(daughter2);
-console.log(`Моя самая любимая кошечка ${daughter2} !!!`);
-console.log(son2);
+// const {daughter2, son2} = object.children;
+// console.log(daughter2);
+// console.log(`Моя самая любимая кошечка ${daughter2} !!!`);
+// console.log(son2);
 
 
 // Удаление свойства из объекта
@@ -396,24 +389,99 @@ console.log(son2);
 
 
 // Разворачивание массива с помощью спред-оператора
-const arr = [1, 5, 10, 15];
+//const arr = [1, 5, 10, 15];
 
 //console.log(arr.at(0));
-console.log(...arr);
+//console.log(...arr);
 
 // Разворачивание объекта с помощью спред-оператора
 
-const abc = {
-    a: 1,
-    b: 'mama',
-    c:21
-};
-
-const ABC = {...abc};
-
-console.log(ABC);
+// const abc = {
+//     a: 1,
+//     b: 'mama',
+//     c:21
+// };
+//
+// const ABC = {...abc};
+//
+// console.log(ABC);
 
 
 // Динамическая типизация
-console.log(typeof(+'num'));
+// console.log(typeof(+'num'));
 
+//console.log(typeof(3 + ''));
+
+
+
+
+
+// Задачи  с собеседований
+
+// 1. Какое будет выведено значение?
+// let x = 5;
+// alert(x++);
+// Ответ 5 так как Постфиксная форма инкремента
+
+
+// 2. Чему равно такое выражение?
+// let a = [ ] + false - null + true;
+
+// console.log([] + false);
+// console.log(typeof([] + false)); // Значение - false, а тип данных - string // Пустой Массив при сложении приводится
+// // к Пустой строке, а при Сложении // Пустой // строки с чем либо (Конкатениция) Всё выражение приводится к Строке!!!
+//
+// console.log([] + false - null); // Значение - Nan
+// console.log(typeof([] + false - null)); // Тип данных - number
+//
+// console.log([] + false - null + true); // Значение - Nan
+// console.log(typeof([] + false - null + true)); // Тип данных - number
+
+// 3. Что выведет этот код? Ответ 2 Последовательное присваивание значения справа
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// 4. Чему равна сумма [] + 1 + 2;?
+// Ответ 12 Тип данных - string
+// console.log([] + 1 + 2);
+// console.log(typeof([] + 1 + 2));
+
+// 5. Что выведет этот код: alert( "1"[0] );?
+// alert("1"[0]);
+// Ответ 1, будет выведен Первый символ(с индексом 0) из строки "1"
+
+// 6. Чему равно 2 && 1 && null && 0 && undefined;?
+//console.log(2 && 1 && null && 0 && undefined);
+// Ответ null, так как оператор && "Запинается на Лжи" (а null даёт значение false) и дальше уже не выполняется,
+// а оператор || "Запинается на Правде"
+
+// 7. Есть ли разница между выражениями? !!( a && b ) и (a && b);?
+// Есть, два !! меняют тип данных на Булевое значение
+
+// 8. Что выведет этот код: alert( null || 2 && 3 || 4 );?
+//alert( null || 2 && 3 || 4 );
+// Ответ 3
+
+// 9. a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b;?
+// let a = [1, 2, 3],
+//     b = [1, 2, 3];
+//
+// console.log(a == b);
+// Ответ a Не равно b так как хоть и элементы массивов равны, но это совершенно РАЗНЫЕ переменные a и b !!!
+
+// 10. Что выведет этот код: alert( +"Infinity" ); ?
+//alert(+"Infinity"); // Значение Infinity
+//alert(typeof(+"Infinity")); // Тип данных number
+
+// 11. Верно ли сравнение: "Ёжик" > "яблоко"; ?
+//console.log("Ёжик" > "яблоко");
+// Ответ Нет.
+// Тут используется Посимвольное сравнение (Если первый символ первой строки больше (меньше),
+// чем первый символ второй, то первая строка больше (меньше) второй. Сравнение завершено.). Используется
+// кодировка Unicode, а не настоящий алфавит. В JavaScript имеет значение регистр символов. Заглавная буква
+// "A" не равна строчной "a". Какая же из них больше? Строчная "a". Почему? Потому что строчные буквы имеют больший код
+// во внутренней таблице кодирования, которую использует JavaScript (Unicode).
+
+// 12. Чему равно 0 || "" || 2 || undefined || true || falsе; ?
+//console.log(0 || "" || 2 || undefined || true || falsе);
